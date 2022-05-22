a32792e93d299e271c5e738d023b9c3745e72ebe
Corentin VIDAL <corentin.vidal@epitech.eu>
2022-05-22 13:42:24 +0200

    Fix: Chanded timings

--- scripts/phil.js
+++ scripts/phil.js
@@ -17,15 +17,16 @@ function init() {
 
   document.getElementById("📀").addEventListener('click', function kill() {
     this.removeEventListener('click', kill);
-    document.getElementById("chromeisdeftheworstbrowserever").style.backgroundImage = "linear-gradient(233deg, #F00, #000, #F00, #000)"
-    document.getElementById("chromeisdeftheworstbrowserever").style.animation = "linear infinite 1s susback"
     document.querySelector("#📀 img").src = "assets/adrien_slash.png";
-    document.getElementById("🔪").style.visibility = "visible";
     let jukebox = document.getElementById("🎶")
     jukebox.loop = false;
     jukebox.src = "assets/slash.mp3";
     jukebox.play();
-    jukebox.addEventListener('ended', () => {
+    jukebox.addEventListener('ended', function alarm() {
+      this.removeEventListener('ended', alarm);
+      document.getElementById("🔪").style.visibility = "visible";
+      document.getElementById("chromeisdeftheworstbrowserever").style.backgroundImage = "linear-gradient(233deg, #F00, #000, #F00, #000)";
+      document.getElementById("chromeisdeftheworstbrowserever").style.animation = "linear infinite 1s susback";
       jukebox.src = "assets/sus.mp3";
       jukebox.loop = true;
       jukebox.play();
@@ -73,4 +74,5 @@ function init() {
   -*%%%%%%%*=.          ##*****##%#\n\
       ..                =%%%%%%@@@@.\n\
                          :-=+***+:");
+  console.info("Ceci est sans nul doute le pire code que j'eusse jamais écrit de l'intégralité de mon existence.")
 }
